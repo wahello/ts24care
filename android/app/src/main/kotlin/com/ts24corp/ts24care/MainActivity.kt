dbab3774f40ed4ef73f63086f7842604ad573ccc
ts24corp <play@ts24.com.vn>
2020-02-26 13:31:03 +0700

    u

---
+++ android/app/src/main/kotlin/com/ts24corp/ts24care/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ts24corp.ts24care
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
